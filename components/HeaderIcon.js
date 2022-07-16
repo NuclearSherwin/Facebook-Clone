7e25376e3ed8f8554493e637c95991b0c1613915
Phong Mai <phongspacenasa@gmail.com>
2022-07-16 21:22:41 +0700

    done center nav

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function HeaderIcon({ Icon }) {
+function HeaderIcon({ Icon, active }) {
   return (
-    <div>
-        <Icon className='h-5'/>
+    <div className='flex items-center cursor-pointer sm:h-10 md:px-10 
+    md:hover:bg-gray-100 rounded-xl active:border-b-2
+     active:border-blue-500 group text-gray-500'>
+        <Icon className={`h-5 md:px-10 sm:h-7 mx-auto
+         group-hover:text-blue-500 ${active && 'text-blue-500'}`}/>
     </div>
   )
 }
