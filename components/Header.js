170f2ed1784fac58e8d43b6fe652650d06d6fd80
Phong Mai <phongspacenasa@gmail.com>
2022-07-18 19:47:43 +0700

    done the header

--- components/Header.js
+++ components/Header.js
@@ -31,7 +31,7 @@ function Header() {
 
             <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
               <SearchIcon className='h-6 text-gray-600'/>
-              <input className='flex ml-2 items-center bg-transparent outline-none
+              <input className='hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none
               placeholder-gray-500 flex-shrink'
                type="text" placeholder='Search' />
             </div>
@@ -49,6 +49,14 @@ function Header() {
         </div>
 
         {/* Right */}
+        <div className='flex items-center sm:space-x-2 justify-end'>
+          {/* Profile picture */}
+          <p className='white-space-nowrap font-semibold pr-3'>Paika</p>
+          <ViewGridIcon className='Icon'/>
+          <ChatIcon className='Icon'/>
+          <BellIcon className='Icon'/>
+          <ChevronDownIcon className='Icon'/>
+        </div>
     </div>
   )
 }
